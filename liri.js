7dca34b5a560483d9a280af656f9fe35a457917f
LMBoyle <boylelukem@gmail.com>
2019-08-25 18:59:58 -0400

    initial files

---
+++ liri.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+// TODO concert-this
+
+// TODO bands API
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// TODO spotify-this-song
+// TODO spotify API
+// TODO "The Sign" by Ace of Base
+
+// TODO movie-this
+// TODO OMDB API
+
+// TODO do-what-it-says
+
