7d050db244ebbe88c36c12e57d9483a20581b216
LMBoyle <boylelukem@gmail.com>
2019-08-25 20:06:56 -0400

    added omdb api and console logs

--- liri.js
+++ liri.js
@@ -2,22 +2,23 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
+const axios = require('axios');
 
 var input = process.argv[2];
 
-// TODO concert-this
+// concert-this
 if (input === "concert-this") {
   displayConcertInfo();
 }
-// TODO spotify-this-song
+// spotify-this-song
 else if (input === "spotify-this-song") {
   displaySpotifyInfo();
 }
-// TODO movie-this
+// movie-this
 else if (input === "movie-this") {
   displayMovieInfo()
 }
-// TODO do-what-it-says
+// do-what-it-says
 else if (input === "do-what-it-says") {
   displayWhatItSays();
 }
@@ -25,41 +26,69 @@ else if (input === "do-what-it-says") {
 // TODO bands API
 // "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 function displayConcertInfo() {
-  console.log("Concert This")
+  console.log("Concert This");
+
+  // TODO Venue
+  // TODO Venue Location
+  // TODO Event Date
+  // TODO Format Event Date
 }
 
 // TODO spotify API
 // TODO "The Sign" by Ace of Base
 function displaySpotifyInfo() {
-  console.log("Spotify This")
+  console.log("Spotify This");
+
+  // TODO Artist(s)
+  // TODO Song Name
+  // TODO Preview Link
+  // TODO Album
 }
 
 // OMDB API
 function displayMovieInfo() {
-  console.log("Movie This")
+  console.log("Movie This");
 
-  /*
   var movie = process.argv[3];
   var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-  // Creating an AJAX call for the specific movie button being clicked
-  $.ajax({
+  axios({
     url: queryURL,
-    method: "GET"
+    method: "get"
   }).then(function(response) {
+    var response = response.data
 
-    var rating = response.Rated;
-
+    // Title
+    var title = response.Title;
+    // Year
     var released = response.Released;
-
+    // IMDB Rating
+    var imdb = response.Ratings[0].Value;
+    // Rotten Tomatoes Rating
+    var tomatoes = response.Ratings[2].Value;
+    // Country Produced
+    var country = response.Country;
+    // Language
+    var lang = response.Language;
+    // Plot
     var plot = response.Plot;
-  
+    // Actors
+    var actor = response.Actors;
 
-
+    // Display
+    console.log("Title: ", title)
+    console.log("Year Released: ", released)
+    console.log("IMDB Rating: ", imdb)
+    console.log("Rotton Tomato Rating: ", tomatoes)
+    console.log("Country Produced: ", country)
+    console.log("Language: ", lang)
+    console.log("Plot: ", plot)
+    console.log("Actor(s): ", actor)
   });
-  */
 }
 
 function displayWhatItSays() {
-  console.log("Do What It Says")
+  console.log("Do What It Says");
+
+  // TODO random.txt
 }
